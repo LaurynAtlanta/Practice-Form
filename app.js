8fc57f933d17ab464e0d54c03d078fefe08b8c2b
“Lauryn <laurynmills111@gmail.com>
2020-09-11 18:28:26 -0400

    animation working for correct length of input

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let form = document.getElementById('form');
 let username= document.getElementById('name');
 let password= document.getElementById('password');
+let inputs = document.getElementsByTagName('input');
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
@@ -11,12 +12,23 @@ form.addEventListener('submit', function(e){
 function checkLength(){
     const usernameValue = username.value;
     const passwordValue = password.value;
-    if(usernameValue< 6){
+    if(usernameValue.length< 6){
         form.style.animation= 'shake 0.5s ease';
-    } else if(passwordValue< 6){
+    } else if(passwordValue.length< 6){
         form.style.animation= 'shake 0.5s ease';
     } 
     form.addEventListener('animationend', () => { 
         form.style.animation = '';
     });
 }
+
+// function checkLength(){
+//     for(let i=0; i<inputs.length; i++){
+//         if(inputs[i].value<6){
+//             form.style.animation= 'shake 0.5s ease';
+//         }
+//     }
+//     form.addEventListener('animationend', () => { 
+//                 form.style.animation = '';
+//             });
+// }
